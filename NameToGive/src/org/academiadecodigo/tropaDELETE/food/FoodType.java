9ab8cced0bfcf56557e0cdba3a3e0b8bb0dbb59e
Ana Da Fonseca <ana.teixeiradafonseca@gmail.com>
2019-02-28 18:36:02 -0100

    Added ellipse as FoodType arguments

--- NameToGive/src/org/academiadecodigo/tropaDELETE/food/FoodType.java
+++ NameToGive/src/org/academiadecodigo/tropaDELETE/food/FoodType.java
@@ -1,19 +1,22 @@
 package org.academiadecodigo.tropaDELETE.food;
 
 import org.academiadecodigo.simplegraphics.graphics.Ellipse;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public enum FoodType { //Ana vai tratar de dar uma ellipse (inicialmente) a cada type
 
-    CARROT(1),
-    HAMBURGER(2),
-    TOMATO(3),
-    PIZZA(4);
+    CARROT(1, new Ellipse(400,450,30,30)),
+    HAMBURGER(2, new Ellipse(400,400,30,30)),
+    TOMATO(3, new Ellipse(400,400,30,30)),
+    PIZZA(4, new Ellipse(400,400,30,30));
 
     Ellipse ellipse;
     int speed;
 
-    FoodType(int speed) {
+
+    FoodType(int speed, Ellipse ellipse) {
         this.speed = speed;
+        this.ellipse = ellipse;
     }
 
     public int getSpeed() {
