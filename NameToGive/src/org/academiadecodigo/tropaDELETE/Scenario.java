066b0c6559b2a196205fe571e1d677ec91666885
RMelo <rmcrazymod@gmail.com>
2019-02-28 19:07:20 -0100

    minor changes

--- NameToGive/src/org/academiadecodigo/tropaDELETE/Scenario.java
+++ NameToGive/src/org/academiadecodigo/tropaDELETE/Scenario.java
@@ -2,7 +2,11 @@ package org.academiadecodigo.tropaDELETE;
 
 public class Scenario {
 
-    
+    public static final int PADDING = 10;
+    public static final int WINDOW_WIDTH = 800 + PADDING;
+    public static final int WINDOW_HEIGHT = 500 + PADDING;
+
+    public static final int
 
 
 }
